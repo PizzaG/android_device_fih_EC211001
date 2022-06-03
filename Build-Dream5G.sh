6915d25346be6650247b831996eff720682f0bc8
PizzaG <49663808+PizzaG@users.noreply.github.com>
2022-06-03 18:08:52 -0500

    Update Build-Dream5G.sh

--- Build-Dream5G.sh
+++ Build-Dream5G.sh
@@ -3,19 +3,15 @@
 ## TWRP
 #############################
 export USE_CCACHE=1
-#export BR_MAINTAINER=PizzaG
 . build/envsetup.sh
 m clean
 lunch twrp_Dream5G-eng
 mka bootimage
-#mka recoveryimage
 echo
 echo " Recovery Should Be Built"
 echo ""
 mv $OUT/boot.img ~/Desktop/TWRP_Boot-Cricket_Dream_5G.img
-#mv $OUT/recovery.tar /media/pizzag/Android/Uploads/Recoveries/TWRP_Recovery-beyond2qlte.tar
-#cd /media/pizzag/Android/Recoveries/TWRP/10
-#make clean
+#m clean
 read
 #############################
 #############################
