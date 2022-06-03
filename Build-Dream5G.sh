eeecf1ce0b0ed8efd5bd309b891a538529cde5cc
PizzaG <49663808+PizzaG@users.noreply.github.com>
2022-06-03 18:13:37 -0500

    Add files via upload

---
+++ Build-Dream5G.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+## SkyHawk
+#############################
+export USE_CCACHE=1
+#export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+#export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
+. build/envsetup.sh
+m clean
+lunch twrp_Dream5G-eng
+#lunch twrp_EC211001-eng
+mka bootimage
+#mka recoveryimage
+echo
+echo " Recovery Should Be Built"
+echo ""
+mv $OUT/boot.img ~/Desktop/SkyHawk_Boot-Cricket_Dream_5G.img
+#mv $OUT/recovery.tar /media/pizzag/Android/Uploads/Recoveries/TWRP_Recovery-beyond2qlte.tar
+#cd /media/pizzag/Android/Recoveries/TWRP/10
+#make clean
+read
+#############################
+#############################
+
