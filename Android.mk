09d39184922429ef715be3ecebe57b97e96852da
lopestorm <lopestom1@gmail.com>
2022-06-04 10:20:07 -0300

    Add files via upload

--- Android.mk
+++ Android.mk
@@ -1,12 +1,5 @@
-#
-# Copyright (C) 2020 The Android Open Source Project
-# Copyright (C) 2020 The TWRP Open Source Project
-# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
-# Copyright (C) 2019-Present A-Team Digital Solutions
-#
-
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE), Dream5G)
+ifeq ($(TARGET_DEVICE), EC211001)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,6 @@
-#
-# Copyright (C) 2020 The Android Open Source Project
-# Copyright (C) 2020 The TWRP Open Source Project
-# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
-# Copyright (C) 2019-Present A-Team Digital Solutions
-#
-
 PRODUCT_MAKEFILES := \
-     $(LOCAL_DIR)/twrp_Dream5G.mk
-
+     $(LOCAL_DIR)/twrp_EC211001.mk
+     
 COMMON_LUNCH_CHOICES := \
-    twrp_Dream5G-eng \
-    twrp_Dream5G-userdebug \
-    twrp_Dream5G-user
+    twrp_EC211001-eng
+    
